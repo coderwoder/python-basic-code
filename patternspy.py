687093997c6a8bc68140e036525ed755b84cd111
coderwoder <shkmd29@gmail.com>
2022-06-01 00:33:35 +0530

    patterns 0.2

--- patternspy.py
+++ patternspy.py
@@ -1,13 +1,71 @@
-# for x in range(1, n):
-# /// |-
+n = int(input("Enter The Number"))
 # /// |--
+# /// |-
+# for x in range(1, n):
 #     for y in range(x):
 #         print("x", end=' ')
 #     print()
 
-# for x in range(n):
 # /// |--
 # /// |-
+# for x in range(n):
 #    for y in range(x, n):
 #         print("x", end=' ')
 #     print()
+
+# for x in range(1, n):
+#     for y in range(x):
+#         print("x", end=' ')
+#     print()
+# for x in range(n):
+#     for y in range(x, n):
+#         print("x", end=' ')
+#     print()
+
+# for x in range(0, n):
+#     for y in range(x, n):
+#         print(" ", end=' ')
+#     for k in range(x+1):
+#         print("*", end=' ')
+#     print()
+
+# for x in range(0, n):
+#     for y in range(x+1):
+#         print(" ", end=' ')
+#     for k in range(x, n):
+#         print("*", end=' ')
+#     print()
+
+# for x in range(0, n):
+#     for y in range(x+1):
+#         print(" ", end=' ')
+#     for y in range(x, n-1):
+#         print("*", end=' ')
+#     for y in range(x, n):
+#         print("*", end=' ')
+#     print()
+# for x in range(0, n):
+#     for y in range(x, n):
+#         print(" ", end=' ')
+#     for y in range(x):
+#         print("*", end=' ')
+#     for y in range(x+1):
+#         print("*", end=' ')
+#     print()
+
+for x in range(0, n-1):
+    for y in range(x, n):
+        print(" ", end=' ')
+    for y in range(x):
+        print("*", end=' ')
+    for y in range(x+1):
+        print("*", end=' ')
+    print()
+for x in range(0, n):
+    for y in range(x+1):
+        print(" ", end=' ')
+    for y in range(x, n-1):
+        print("*", end=' ')
+    for y in range(x, n):
+        print("*", end=' ')
+    print()
