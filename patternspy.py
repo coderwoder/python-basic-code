fa043b8f6808abdaaca630981e1e4b9953577e54
coderwoder <shkmd29@gmail.com>
2022-05-31 23:49:02 +0530

    pattern

---
+++ patternspy.py
@@ -0,0 +1,13 @@
+# for x in range(1, n):
+# /// |-
+# /// |--
+#     for y in range(x):
+#         print("x", end=' ')
+#     print()
+
+# for x in range(n):
+# /// |--
+# /// |-
+#    for y in range(x, n):
+#         print("x", end=' ')
+#     print()
